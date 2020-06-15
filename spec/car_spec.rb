373c6b9d870fc0db9bba7f01a7a6aacd1d35114a
Sima <sima.boreiri@gmail.com>
2020-06-15 02:17:21 +0200

    write the test for changing color of car but failed

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,4 +1,8 @@
-require './lib/car.rb'
+require './cars/car.rb'
+
 describe Car do 
-    
+
+    it 'should be able to give a new color to a car' do
+        expect(subject.paint_car("red")).to eq red 
+    end
 end 
